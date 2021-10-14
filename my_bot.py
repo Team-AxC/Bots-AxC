b96b577676c6f2b1b02d3716d4d8ccd7b85bc5f5
Chinmay Krishna <79033720+97I8TOE47K@users.noreply.github.com>
2021-10-14 19:48:01 +0530

    Added activity: Listening to $help

--- my_bot.py
+++ my_bot.py
@@ -10,17 +10,11 @@ from keep_alive import *
 
 
 client = discord.Client()
-api_key = "api_key"
-base_url = "http://api.openweathermap.org/data/2.5/weather?q=Lucknow&appid='apikey'"
+api_key = "API_KEY"
 cmd1 = commands.Bot(command_prefix='$')
 cmds = '$hello\n$name\n$version\n$info\n$help\n$inspire\n$devinfo\n$joke\n$cat_fact\n$weather (Syntax: `$weather [City]`, e.g. `$weather Lucknow`)'
 
-#The bot is listening to commands and nerdy stuff
-@client.event
-async def on_ready():
-   await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=f'{cmd1}[location]'))
 
-        
 
 #functions to be performed
 def get_quote():
@@ -30,7 +24,7 @@ def get_quote():
   return(quote)
 
 def get_joke():
-    response = requests.get("https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,racist&type=single")
+    response = requests.get("https://v2.jokeapi.dev/joke/Any?blacklistFlags=racist&type=single")
     json_data = json.loads(response.text)
     joke = json_data["joke"]
     return (joke)    
@@ -40,7 +34,10 @@ def get_joke():
 #on ready function
 @client.event
 async def on_ready():
+  #The bot is watching commands and nerdy stuff
+  await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="$help"))
   print('ICBM launched by {0.user}, expect destruction soon (of your brain). Dimag Tikka Order being made...'.format(client))
+
 
 #executables
 @client.event
@@ -49,25 +46,25 @@ async def on_message(message):
     return
 
   if message.content.startswith('$hello'):
-    await message.channel.send('Hello there! I am a friendly bot made by the owner of the server @abhishek')
+    await message.channel.send('Hello there! I am a friendly, general-purpose, nerdy bot made by abhishek#4309')
 
   if message.content.startswith('$name'):
-    await message.channel.send('Hello my name is Mr_bot')
+    await message.channel.send('Hello my name is AxC 777')
 
   if message.content.startswith('$version'):
     my_embed = discord.Embed(title = "Current version", description = "The bot is in version 0.1, currently in Pre-Alpha stage of development", color = 0x00ff00)
     my_embed.add_field(name ="version code:", value ="v0.1", inline=False)
-    my_embed.add_field(name = "Date Released:", value="Sep 19", inline=False)
+    my_embed.add_field(name = "Time Released:", value="Sep 21", inline=False)
     my_embed.set_footer(text="")
-    my_embed.set_author(name="@abhishek")
+    my_embed.set_author(name="abhishek#4309")
     await message.channel.send(embed = my_embed)
 
   if message.content.startswith('$info'):
     my_embed = discord.Embed(title = "Name:", description = "AxC 777", color = 0x00ff00)
-    my_embed.add_field(name ="Description:", value ="A Genral Purpose Discord Bot, currently in it's infancy, made by abhishek", inline=False)
+    my_embed.add_field(name ="Description:", value ="A Genral Purpose Discord Bot, currently in it's infancy, made by Abhishek", inline=False)
     my_embed.add_field(name = "Version Info:", value="0.1 (Pre-Alpha)" )
     my_embed.set_footer(text="")
-    my_embed.set_author(name="@abhishek")
+    my_embed.set_author(name="abhishek#4309")
     await message.channel.send(embed = my_embed)
     
   if message.content.startswith('$devinfo'):
@@ -79,7 +76,7 @@ async def on_message(message):
 
   if message.content.startswith('$help'):
     my_embed = discord.Embed(title = "All commands:", description = cmds, color = 0x00ff00)
-    my_embed.set_author(name="@abhishek")
+    my_embed.set_author(name="abhishek#4309")
     await message.channel.send(embed = my_embed)
 
   if message.content.startswith('$inspire'):
@@ -98,7 +95,7 @@ async def on_message(message):
 
   if message.content.startswith('$weather'):
             location = message.content.replace("$weather ", '')
-            url = f'http://api.openweathermap.org/data/2.5/weather?q={location}&appid=53f581dd965b2b09bd6c2ebcb5ce41fd&units=metric'
+            url = f'http://api.openweathermap.org/data/2.5/weather?q={location}&appid={api_key}&units=metric'
             try:
                 data = parse_data(json.loads(requests.get(url).content)['main'])
                 await message.channel.send(embed=weather_message(data, location))
@@ -106,5 +103,5 @@ async def on_message(message):
                 await message.channel.send(embed=error_message(location))
 
 
-keep_alive()
+#keep_alive()
 client.run('Bot Token')
