20a2d7244ca279548ae66145853c0b821169e109
abhishek_saxena <90091440+chinmoysir@users.noreply.github.com>
2021-10-26 21:16:27 +0530

    Update my_bot.py

--- my_bot.py
+++ my_bot.py
@@ -1,24 +1,19 @@
+import os
 import discord
-from discord import message
 from discord import *
-from discord.ext import commands
 import requests
 import json
-from pprint import pprint
+from random import randrange
+from pprint import *
 from weather import *
 from keep_alive import *
 
-
-
 client = discord.Client()
-client_id = 'BOT_TOKEN'
-api_key = "API KEY"
-cmd1 = commands.Bot(command_prefix='$')
-cmds = '$hello\n$name\n$version\n$info\n$help\n$inspire\n$devinfo\n$joke\n$cat_fact\n$weather (Syntax: `$weather [City]`, e.g. `$weather Lucknow`)\n||$secret_command (Type `$hint` for a hint)||'
+my_secret = os.environ['TOKEN']
+api_key = "53f581dd965b2b09bd6c2ebcb5ce41fd"
+cmds = '$hello\n$name\n$version\n$info\n$help\n$inspire\n$devinfo\n$joke\n$cat_fact\n$weather (Syntax: `$weather [City]`, e.g. `$weather Lucknow`)\n||$secret_command (Type `$hint` for a hint)||\n'
 
-
-
-        
+music_cmds = '`?play [with music name]` (the bot will automatically join your voice channel in the server, and the music will be added to the queue)\n`?queue` (will work after 2 songs are added to the queue)\n`?skip` (to go to the next song of the queue)\n`?pause` (to pause)\n`?resume` (to resume)\n`?stop` (to stop the song)\n `?url [with url of YouTube video]` (to play the sound of a youtube video)\n`?disconnect` (to disconnect the bot from the voice channel)'
 
 #functions to be performed
 def get_quote():
@@ -27,13 +22,14 @@ def get_quote():
   quote = json_data[0]['q'] + " -" + json_data[0]['a']
   return(quote)
 
+'''def get_joke1():
+  # https://official-joke-api.appspot.com/random_ten'''
+
 def get_joke():
-    response = requests.get("https://v2.jokeapi.dev/joke/Any?blacklistFlags=racist&type=single")
-    json_data = json.loads(response.text)
-    joke = json_data["joke"]
-    return (joke)    
-
-
+  response = requests.get("https://v2.jokeapi.dev/joke/Any?blacklistFlags=racist&type=single")
+  json_data = json.loads(response.text)
+  joke = json_data["joke"]
+  return (joke)
 
 #on ready function
 @client.event
@@ -41,7 +37,6 @@ async def on_ready():
   #The bot is watching commands and nerdy stuff
   await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="$help"))
   print('ICBM launched by {0.user}, expect destruction soon (of your brain). Dimag Tikka Order being made...'.format(client))
-
 
 #executables
 @client.event
@@ -80,6 +75,7 @@ async def on_message(message):
 
   if message.content.startswith('$help'):
     my_embed = discord.Embed(title = "All commands:", description = cmds, color = 0x00ff00)
+    my_embed.add_field(name = "\nMusic Commands for AxC 777 Music\n(make sure that the music bot is in the server)", value=music_cmds, inline=False)
     my_embed.set_author(name="abhishek#4309")
     await message.channel.send(embed = my_embed)
 
@@ -91,10 +87,10 @@ async def on_message(message):
     joke = get_joke()
     await message.channel.send(joke)
 
-  if message.content.startswith('$bhaiyon_aur_behnon'):
-    my_embed = discord.Embed(title = "100% Real Modi Ji Announcement:", description="Mitron")
-    my_embed.add_field(name = "Ghoshna:", value = "Asha Drugs lene ke baad mujhe yah ahsaas hua hai ki Asha nashe bahut achche nashe hain aur cringe hona bahut zaroori hai. **Isliye aaj Asha Coins launch hone jaa rahe hain, aur Bharat ki aadhikarik mudra ab Asha Coins hi rahegi.**", inline=False)
-    my_embed.add_field(name = "Dhanyavaad!", value = "(Sirf Bharat vassiyon ke liye)", inline = False)
+  if message.content.startswith('$535'):
+    my_embed = discord.Embed(title = "YOU HAVE CRACKED the **DA VINCI CODE**", description="mitron tumne kar dikhaya")
+    my_embed.add_field(name = "Reward:", value = "to achieve the reward is to become the server owner tell your name [in this GOOGLE FORM](https://youtu.be/dQw4w9WgXcQ)", inline=False)
+    my_embed.add_field(name = "Dhanyavaad!", value = "App hee ke vajheh se desh chal raha hai", inline = False)
     await message.channel.send(embed = my_embed)
 
   if message.content.startswith('$cat_fact'):
@@ -104,19 +100,19 @@ async def on_message(message):
     await message.channel.send(embed=embed)   
 
   if message.content.startswith('$hint'):
-    my_embed = discord.Embed(title='Hint for the secret command', description='NTlhNzBkNGEzZjU3NjdkNmRiZWE0MGVjYzlmNGZjYzk5MmZmYzJhMmNiYjhhZjE5ODkyYmVhOTVjMDI0ZmEzMA==') 
-    my_embed.add_field(name='Hint for the secret code #2', value="It's 64 not 69, whole heatedly loved by 16 exponents",inline=False)
+    my_embed = discord.Embed(title='Hint for the secret command', description='18, 9, 3, 11', colour = 0x400080) 
+    my_embed.add_field(name='.', value="add the squares of each number and perform a *secret operation*",inline=False)
     await message.channel.send(embed=my_embed)
 
-  if message.content.startswith('$weather'):
-            location = message.content.replace("$weather ", '')
-            url = f'http://api.openweathermap.org/data/2.5/weather?q={location}&appid={api_key}&units=metric'
-            try:
-                data = parse_data(json.loads(requests.get(url).content)['main'])
-                await message.channel.send(embed=weather_message(data, location))
-            except KeyError:
-                await message.channel.send(embed=error_message(location))
 
+  if message.content.startswith('$weather'):
+    location = message.content.replace("$weather ", '')
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={location}&appid={api_key}&units=metric'
+    try:
+      data = parse_data(json.loads(requests.get(url).content)['main'])
+      await message.channel.send(embed=weather_message(data, location))
+    except KeyError:
+      await message.channel.send(embed=error_message(location))
 
 keep_alive()
-client.run(client_id)
+client.run(my_secret)
