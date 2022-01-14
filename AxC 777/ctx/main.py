3df1fcc6784447d23bd49b094506fce74348e785
Chinmay Krishna <79033720+97I8TOE47K@users.noreply.github.com>
2022-01-14 17:37:30 +0530

    Update main.py

--- AxC 777/ctx/main.py
+++ AxC 777/ctx/main.py
@@ -5,6 +5,8 @@ import os
 import requests
 import json
 from keep_alive import *
+from discord import User
+from discord.ext.commands import Bot, guild_only
 
 #defining the command variable
 client = commands.Bot(command_prefix = '$')
@@ -32,9 +34,10 @@ async def intro(ctx):
   grogu_hello = "https://tenor.com/view/mandalorian-baby-yoda-hello-gif-19013340"
 
   intro_embed = discord.Embed(title = "Essential Introduction", description = "Hey there! I am AxC 777. I am very nerdy 🤓, and made by Abhishek, in collaboration with Chinmay. I am meant to be general purpose with **a lot** of features being worked on and should be added down the road!")
-  intro_embed.add_field(name = "Version 0.2", value = "Development stage: Beta", inline=False)
+  intro_embed.add_field(name = "Version 0.3", value = "Development stage: Beta", inline=False)
+  intro_embed.add_field(name = "GitHub Repo :ninja:", value = "https://github.com/chinmoysir/DISCORD-BOT")
   intro_embed.add_field(name = "Release Time", value = "September 2021", inline = False)
-  intro_embed.add_field(name = "Use the `$assist` command for the list of available commands ", value = "-developers", inline=False)
+  intro_embed.add_field(name = "Use the `$assist` command for the list of available commands ", value = "\u200b", inline=False)
   await ctx.send(grogu_hello)
   await ctx.send(embed = intro_embed)
 
@@ -80,6 +83,21 @@ async def ban(ctx, member : discord.Member,*, reason = "*No specific reason prov
   await member.ban(reason = reason)
 
 @client.command()
+@commands.has_permissions(ban_members = True)
+async def unban(ctx,*,member):
+  banned_users = await ctx.guild.bans()
+  member_name , member_disc = member.split('#')
+
+  for banned_entry in banned_users:
+    user = banned_entry.user
+
+    if(user.name, user.discriminator) == (member_name,member_disc):
+      await ctx.guild.unban(user)
+      await ctx.send(member_name + " has been unbanned!")
+
+  await ctx.send(member+" was not found")
+
+@client.command()
 async def warn(ctx, member : discord.Member,*, reason = "*No specific reason provided to by the moderator*"):
   my_embed = discord.Embed(title = f"{ctx.author} has warned {member}",colour=0x400080)
   my_embed.add_field(name = "Reason:", value= reason, inline=False)
@@ -91,17 +109,7 @@ async def assist(ctx):
     my_embed.add_field(name = "\n\nMusic Commands for AxC 777 Music\n(make sure that the music bot is in the server)", value=music_cmds, inline=False)
     my_embed.set_author(name="Abhishek Saxena (https://github.com/chinmoysir)")
     await ctx.send(embed = my_embed)
-
-# @client.command()
-# async def weather(ctx, *args):
-#   location = args
-#   url = f'http://api.openweathermap.org/data/2.5/weather?q={location}&appid={open_weather_api_key}&units=metric'
-#   try:
-#       data = parse_data(json.loads(requests.get(url).content)['main'])
-#       await ctx.send(embed=weather_message(data, location))
-#   except KeyError:
-#       await ctx.send(embed=error_message(location))
-
+    
 keep_alive()
 my_secret = os.environ['BOT']
 client.run(my_secret)
