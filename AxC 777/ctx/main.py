05d50eff3f1c009d53c75acdf580df13146c318c
Chinmay Krishna <79033720+97I8TOE47K@users.noreply.github.com>
2022-01-15 19:22:53 +0530

    Update main.py

--- AxC 777/ctx/main.py
+++ AxC 777/ctx/main.py
@@ -13,7 +13,8 @@ client = commands.Bot(command_prefix = '$')
 open_weather_api_key = os.environ['weather_api_key']
 cmds = '**REGULAR COMMANDS:**\n$intro\n$help\n$inspire\n$devinfo\n$joke\n$cat_fact\n$weather (Syntax: `$weather [City]`, e.g. `$weather Lucknow`)\n$convert [original temperature unit] [desired temperature unit] [numeral temperature value] e.g. `$convert F C 212` \n**Note:** Distance conversation in beta. Only conversion from `m` to `km` currently available. The format is the same as the temperature conversion syntax, e.g. `$convert m km 50`\n$spam [message], e.g. `$spam Cool Science`\n\n**MODERATION COMMANDS:**\n$kick\n$ban\n$warn'
 
-music_cmds = "`?play [with audio title]` (the bot will automatically join your voice channel in the server, and the audio will be added to the queue)\n`?lyrics [song title]` (will show the lyrics of the song)\n`?queue` \n`?skip` (to play the next song of the queue)\n`?pause`\n`?resume`\n`?stop`\n `?url [with the URL of the YouTube video]` (to play the sound of a YouTube video)\n`?loop [audio title] [looping constant (no. of times for the audio to loop)]` (to loop music n number of times)\n`?loop_10 [audio title]` (to loop music 10 times)\n`?disconnect` or `?dc` (to disconnect the bot from the voice channel)\n`?clear` (to clear the queue)"
+music_cmds = "`?play [audio title]` (the bot will automatically join your voice channel in the server, and the audio will be added to the queue)\n`?lyrics [song title]` (will show the lyrics of the song)\n`?queue` \n`?skip` (to play the next song of the queue)\n`?pause`\n`?resume`\n`?stop`\n `?url [URL of the YouTube video]` (to play the sound of a YouTube video)\n`?loop [audio title] [looping constant (no. of times for the audio to loop)]` (to loop music n number of times)\n`?loop_10 [audio title]` (to loop music 10 times)\n`?disconnect` or `?dc` (to disconnect the bot from the voice channel)\n`?clear` (to clear the queue)"
+
 
 #functions
 def get_quote():
@@ -34,12 +35,18 @@ async def intro(ctx):
   grogu_hello = "https://tenor.com/view/mandalorian-baby-yoda-hello-gif-19013340"
 
   intro_embed = discord.Embed(title = "Essential Introduction", description = "Hey there! I am AxC 777. I am very nerdy 🤓, and made by Abhishek, in collaboration with Chinmay. I am meant to be general purpose with **a lot** of features being worked on and should be added down the road!")
-  intro_embed.add_field(name = "Version 0.3", value = "Development stage: Beta", inline=False)
+  intro_embed.add_field(name = "Version 0.3a", value = "Development stage: Beta", inline=False)
   intro_embed.add_field(name = "GitHub Repo :ninja:", value = "https://github.com/chinmoysir/DISCORD-BOT")
-  intro_embed.add_field(name = "Release Time", value = "September 2021", inline = False)
+  intro_embed.add_field(name = "Release Month :calendar_spiral:", value = "September 2021", inline = False)
   intro_embed.add_field(name = "Use the `$assist` command for the list of available commands ", value = "\u200b", inline=False)
   await ctx.send(grogu_hello)
   await ctx.send(embed = intro_embed)
+
+@client.command()
+async def namaste(ctx, member : discord.Member):
+  await ctx.send(f"AxC777 says namaste to {member} on behalf of {ctx.author}")
+
+  #it works !! chinmoy see chat, thanks btw 😀
 
 @client.command()
 async def dev_info(ctx):
@@ -79,8 +86,10 @@ async def kick(ctx, member : discord.Member,*, reason = "*No specific reason pro
 @client.command()
 @commands.has_permissions(ban_members = True)
 async def ban(ctx, member : discord.Member,*, reason = "*No specific reason provided to by the moderator*"):
-  await member.send("You have been banned from a server, because:"+reason)
+  await member.send(f"You have been banned from a server by {ctx.author}, because:"+reason)
   await member.ban(reason = reason)
+
+  ctx.send(f"{ctx.member} has banned and kicked by {ctx.author}")
 
 @client.command()
 @commands.has_permissions(ban_members = True)
@@ -95,9 +104,11 @@ async def unban(ctx,*,member):
       await ctx.guild.unban(user)
       await ctx.send(member_name + " has been unbanned!")
 
-  await ctx.send(member+" was not found")
+    else:
+      await ctx.send(member+" was not found")
 
 @client.command()
+@commands.has_permissions(ban_members = True)
 async def warn(ctx, member : discord.Member,*, reason = "*No specific reason provided to by the moderator*"):
   my_embed = discord.Embed(title = f"{ctx.author} has warned {member}",colour=0x400080)
   my_embed.add_field(name = "Reason:", value= reason, inline=False)
@@ -109,6 +120,8 @@ async def assist(ctx):
     my_embed.add_field(name = "\n\nMusic Commands for AxC 777 Music\n(make sure that the music bot is in the server)", value=music_cmds, inline=False)
     my_embed.set_author(name="Abhishek Saxena (https://github.com/chinmoysir)")
     await ctx.send(embed = my_embed)
+
+
     
 keep_alive()
 my_secret = os.environ['BOT']
