2f4f211b49026a4980aca68d2a91a1cc62c0fd02
Chinmay Krishna <79033720+97I8TOE47K@users.noreply.github.com>
2022-02-06 18:25:14 +0530

    Update music_cog.py

--- AxC 777 Music/music_cog.py
+++ AxC 777 Music/music_cog.py
@@ -1,5 +1,7 @@
 import discord
 from discord.ext import commands
+import spotipy
+from spotipy.oauth2 import *
 from youtube_dl import YoutubeDL
 import youtube_dl
 from lyrics_extractor import SongLyrics
@@ -9,7 +11,22 @@ from random import *
 from sound_tinkerlab import *
 
 
-music_cmds = "`?play [audio title]` (the bot will automatically join your voice channel in the server, and the audio will be added to the queue)\n`?lyrics [song title]` (will show the lyrics of the song)\n`?queue` \n`?skip` (to play the next song of the queue)\n`?pause`\n`?resume`\n`?stop`\n`?url [URL of the YouTube video]` (to play the sound of a YouTube video)\n`?loop [audio title] [looping constant (no. of times for the audio to loop)]` (to loop music n number of times)\n`?loop_10 [audio title]` (to loop music 10 times)\n`?disconnect` or `?dc` (to disconnect the bot from the voice channel)\n`?clear` (to clear the queue)\n`?fft [wav, mp3 or ogg attachment]` (Fast Fourier Transforms and sends the plot)"
+music_cmds = "`?play [audio title]` (the bot will automatically join your voice channel in the server, and the audio will be added to the queue)\n`?lyrics [song title]` (will show the lyrics of the song)\n`?queue` \n`?skip` (to play the next song of the queue)\n`?pause`\n`?resume`\n`?stop`\n`?url [URL of the YouTube video]` (to play the sound of a YouTube video)\n`?loop [audio title] [looping constant (no. of times for the audio to loop)]` (to loop music n number of times)\n`?loop_10 [audio title]` (to loop music 10 times)\n`?disconnect` or `?dc` (to disconnect the bot from the voice channel)\n`?clear` (to clear the queue)\n"
+
+scientific_cmds = "`?fft [wav, mp3 or ogg attachment]` (Fast Fourier Transforms and sends the plot)"
+
+sp_clientid = os.environ['SPOTIFY_CLIENTID']
+sp_clientsecret = os.environ['SPOTIFY_CLIENTSECRET']
+
+
+json_api_key = os.environ['GCS_JSON_API']
+gcs_genius_engineid = os.environ['GCS_GENIUS_ENGINE_ID']
+
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=sp_clientid, client_secret=sp_clientsecret))
+
+
+
+
 
 
 class music_cog(commands.Cog):
@@ -87,7 +104,7 @@ class music_cog(commands.Cog):
         if voice_channel is None:
             await ctx.send("Connect to a voice channel!")
         else:
-            # if (ctx.author == "Abhishek Saxena (https://github.com/chinmoysir)"):
+            # if (ctx.author == "Abhishek Saxena ()"):
             #         await ctx.send("RICKLOCKED 🔐\nNo more rickrolls allowed")
 
             song = self.search_yt(query)
@@ -110,6 +127,7 @@ class music_cog(commands.Cog):
 
                 if self.is_playing == False:
                     await self.play_music()
+
 
     @commands.command(name="queue", help="Displays the current songs in queue")
     async def queue(self, ctx):
@@ -204,11 +222,17 @@ class music_cog(commands.Cog):
 
     @commands.command()
     async def help(self, ctx):
-        self.my_embed = discord.Embed(title="All commands:",
-                                      description=music_cmds,
+        self.my_embed = discord.Embed(title="",
+                                      description= "",
                                       color=0x00ff00)
+
+        self.my_embed.add_field(name = "Regular Cmds:" , value = music_cmds, inline = False)
+
+        self.my_embed.add_field(name = "Spotify Integrated Cmds" , value = "`?top_tracks [artist name]`", inline = True)
+
+        self.my_embed.add_field(name = "Scientific Cmds" , value = scientific_cmds, inline = True)        
         self.my_embed.set_author(
-            name="author - Abhishek Saxena (https://github.com/chinmoysir)")
+            name = "AxC 777 Music" , icon_url = "https://images.unsplash.com/photo-1614680376573-df3480f0c6ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=80&q=80")
         await ctx.send(embed=self.my_embed)
 
     @commands.command()
@@ -321,6 +345,8 @@ class music_cog(commands.Cog):
       my_embed.set_image(url="attachment://fft.png")
       await ctx.send(file = file, embed = my_embed)
 
+
+
     @commands.command()
     async def fft(self, ctx):
       if str(ctx.message.attachments) == "[]": 
@@ -352,3 +378,32 @@ class music_cog(commands.Cog):
         else:
             await ctx.send("File type not supported")
 
+    # @commands.command()
+    # async def find_artist(self, ctx, *args):
+    #   args_list = list(args)
+
+    #   sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=sp_clientid, client_secret=sp_clientsecret))
+
+    #   if args_list[0] == 'find':
+    #     artist_id = args_list[1]
+    #     spotify_artist = sp.artist(artist_id)
+
+    #     await ctx.send(f"**{spotify_artist}** is the Spotify artist_id with {artist_id} ID")
+
+    @commands.command(name = "top_tracks")
+    async def top_tracks(self, ctx, *args):
+        artist = " ".join(args)
+  
+        results = sp.search(q=artist, limit=10, type="track")
+
+        self.my_embed = discord.Embed(title = f"Top tracks of {artist.title()}", color = 0x00ff00)
+
+        for idx, track in enumerate(results['tracks']['items']):
+            min_sec = divmod(track['duration_ms'] / 1000, 60)
+            
+            self.my_embed.add_field(name = f"{idx + 1}. {track['name']}", value = f"**Duration:** {int(min_sec[0])} min {round(min_sec[1],2)} s", inline = False)
+            
+        # print(results)
+
+        await ctx.send(embed = self.my_embed)
+        
